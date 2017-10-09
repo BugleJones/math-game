b0c9116abc176fe3ca895c06808776acb35a8422
BugleJones <spencerjhamilton@gmail.com>
2017-10-09 21:48:23 +0000

    Extracted relevant nouns and have important details planned

---
+++ math_game.rb
@@ -0,0 +1,20 @@
+# Planning Stage
+
+# Players
+## Two players 
+## on turn -> answer math question
+## before turn -> pick number between 1...20
+## Start with 3 lives
+
+# Turns
+## Player answers question
+## Player is right || player loses life
+## End of every turn output the scores for each player
+
+# Questions
+## Simple math questions
+
+# Math game
+## Start with 3 lives each
+## One player loses all lives
+## Output scores
